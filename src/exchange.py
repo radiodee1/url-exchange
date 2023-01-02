8ef55b0cb2c1382da3cae2e8afbceeba158f1f86
David Liebman <radiodee1@gmail.com>
2023-01-02 14:51:46 -0500

    more exchange stuff - no file loading yet

--- src/exchange.py
+++ src/exchange.py
@@ -7,8 +7,13 @@ class Exchange:
         self.exchange = {}
         self.dict_name = ""
         self.text_name = ""
+        self.wizard = ['radio', 'timer']
+        self.verbose = False
+        self.update = False 
 
     def save_dict(self):
+        if self.update == False:
+            return 
         pass 
 
     def load_dict(self):
@@ -17,8 +22,20 @@ class Exchange:
     def load_txt(self):
         if self.text_name == "" or self.text_name == None:
             return
-        print("load", self.text_name)
+        if self.verbose:
+            print("load", self.text_name)
         pass
+
+    def load_wizards(self):
+        for x in self.wizard:
+            if self.verbose:
+                print("load wizard", x)
+
+    def set_verbose(self, v):
+        self.verbose = v
+
+    def set_update_on_exit(self, update):
+        self.update = update
 
     def set_dict_name(self, dict_name):
         self.dict_name = dict_name;
@@ -26,20 +43,39 @@ class Exchange:
     def set_text_name(self, text_name):
         self.text_name = text_name
 
+    def set_extra_wizards(self, wizards):
+        w = wizards.split(',')
+        for x in w:
+            if x.strip() not in self.wizard:
+                self.wizard.append(x.strip())
+        if self.verbose:
+            print(self.wizard)
+
 
 if __name__ == '__main__':
     e = Exchange()
     parser = argparse.ArgumentParser(description="URL Exchange", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--dict_name', default='./../data/dict.txt', help='name for "dictionary" input file.')
     parser.add_argument('--text_name', help='name for additional "tsv" input file.')
+    parser.add_argument("--wizards", default="radio,timer", help="csv list of possible wizards - added to current list.")
+    parser.add_argument("--verbose", action="store_true", help="show debugging output.")
+    parser.add_argument("--update", action="store_true", help="do not skip updating exchange on exit.")
     args = parser.parse_args()
 
-    print(args.text_name)
-    print(args.dict_name)
+    if args.verbose:
+        print(args.text_name)
+        print(args.dict_name)
 
+    e.set_verbose(args.verbose) 
     e.set_dict_name(args.dict_name)
+    e.set_extra_wizards(args.wizards)
+    e.set_update_on_exit(args.update)
 
     e.load_dict()
     if args.text_name != None:
         e.set_text_name(args.text_name)
         e.load_txt()
+
+    e.load_wizards()
+
+    e.save_dict()
